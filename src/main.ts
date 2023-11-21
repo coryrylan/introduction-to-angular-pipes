5bce37819cb04f83eae489a71ab79fc5addf9c22
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-21 00:10:34 -0600

    5 files modified, 4 added, 7 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
-import './polyfills';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import { EllipsisPipe } from './ellipsis.pipe';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [EllipsisPipe, CommonModule],
+  template: `
+    <h1>Introduction to Angular Pipes</h1>
 
-import { AppModule } from './app/app.module';
+    <p>
+      <a href="https://coryrylan.com/blog/introduction-to-angular-pipes">Tutorial at coryrylan.com</a>
+    </p>
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+    <p>{{myDate}}</p>
 
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
+    <p>{{myDate | date:'shortDate'}}</p>
+
+    <p>{{myDate | date:'longDate'}}</p>
+
+    <p>{{longText | ellipsis: 50 }}</p>
+  `,
+})
+export class App {
+  myNumber = 123456;
+  myDate = new Date();
+  longText = 'Bacon ipsum dolor amet bacon t-bone salami, flank capicola. Leberkas ribeye pork pork loin. Biltong porchetta picanha capicola tri-tip boudin. Tenderloin leberkas chicken, ham pig pork loin flank salami ham hock chuck meatball kevin. Meatloaf capicola landjaeger ground round ham hock ball tip boudin shank pork chop ribeye rump frankfurter turkey. Spare ribs short loin pork chop, biltong capicola shoulder pig drumstick pork porchetta brisket venison turducken sausage. Pig alcatra short loin jowl, prosciutto leberkas ham chuck.';
+}
+
+bootstrapApplication(App);
