ad68f36d506388ed95e30c1a0e6f77877111e125
coryrylan <git@stackblitz.com>
2025-06-01 21:11:48 -0500

    2 files modified, 1 added, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
 import { EllipsisPipe } from './ellipsis.pipe';
 
 @Component({
